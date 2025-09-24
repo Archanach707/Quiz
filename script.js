188891086e56722c0ebfdc9cd3e9cb1121c869ab
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2025-09-24 17:31:18 +0530

    by js added timer count down after 15s change bg color and timer color and again last 5s change gb and timer color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const img = document.querySelector('.quiz-logo');
 const quizBtn = document.querySelector('.button-bg');
 const quizHeading = document.querySelector('.start-quiz');
+const timerBackground = document.querySelector('.timer-wrapper');
 const timerElement = document.querySelector('.timer');
 
 let timeLeft = 30; // seconds
@@ -16,27 +17,43 @@ quizBtn.addEventListener('click', () => {
   document.body.style.backgroundColor = ("#CCE2C2");
 
   // start timer
-  // startTimer();
+  startTimer();
 });
 
-// function startTimer() {
-//   timerElement.textContent = `00:30`; // starting point
+function startTimer() {
+  timerElement.textContent = `00:30`; // starting point
 
-//   const countdown = setInterval(() => {
-//     timeLeft--;
-//     timerElement.textContent = `00:${timeLeft < 10 ? "0" + timeLeft : timeLeft}`;
-
-//     if (timeLeft <= 0) {
-//       clearInterval(countdown);
-//       timerElement.textContent = "00:00";
-//       alert("⏰ Time's up!");
-//     }
-//   }, 1000);
-// }
-
-setInterval(() => {
-  if (timeLeft >= 0) {
-    timerElement.textContent = `00:${timeLeft < 10 ? "0" + timeLeft : timeLeft}`;
+  const countdown = setInterval(() => {
     timeLeft--;
-  }
-}, 1000);
+    timerElement.textContent = `00:${timeLeft < 10 ? "0" + timeLeft : timeLeft}`;
+
+    // 15 seconds condition
+    if (timeLeft === 15) {
+      document.body.style.backgroundColor = "#E4E5C7";
+      timerElement.style.backgroundColor = " #C5B1006E";
+      timerBackground.style.backgroundColor = " #C5B1006E";
+    }
+
+    // 5 seconds condition
+    if (timeLeft === 5) {
+      document.body.style.backgroundColor = "#DBADAD";
+      timerElement.style.backgroundColor = "#C50C006E";
+      timerBackground.style.backgroundColor = "#C50C006E";
+      clearInterval(countdown);
+      timerElement.textContent = "00:00";
+    }
+
+    // if (timeLeft <= 0) {
+    //   clearInterval(countdown);
+    //   timerElement.textContent = "00:00";
+    //   alert("⏰ Time's up!");
+    // }
+  }, 1000);
+}
+
+// setInterval(() => {
+//   if (timeLeft >= 0) {
+//     timerElement.textContent = `00:${timeLeft < 10 ? "0" + timeLeft : timeLeft}`;
+//     timeLeft--;
+//   }
+// }, 1000);
